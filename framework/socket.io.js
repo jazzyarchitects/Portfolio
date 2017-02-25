6ad89388ac18810132ae7aae8d10e56cf92f15d8
Jibin Mathews <jibinmathews7@gmail.com>
2017-02-25 22:36:59 +0530

    Fixing socket

--- framework/socket.io.js
+++ framework/socket.io.js
@@ -2,7 +2,10 @@
 
 module.exports = (server)=>{
   let io = require('socket.io')(server);
+  io.set('transports', ['websocket', 'flashsocket', 'htmlfile', 'xhr-polling', 'jsonp-polling', 'polling']);
+  io.set('origins', '*:*');
   io.sockets.on('connection', (socket)=>{
+    console.log("Socket connected");
     socket.on('notification', (data)=>{
       console.log("Socket:"+JSON.stringify(data));
       socket.broadcast.emit('notification', data);
