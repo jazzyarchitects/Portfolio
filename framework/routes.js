e6e611a495c5a235724dc6ef528867861754a128
Jibin Mathews <jibinmathews7@gmail.com>
2017-02-28 08:10:53 +0530

    Fixing routes

--- framework/routes.js
+++ framework/routes.js
@@ -9,22 +9,26 @@ module.exports = function(app) {
 
   let moduleDirectory = path.join(__dirname, '..', "./modules");
   fs.readdirSync(moduleDirectory).forEach(function(model) {
-    let routesPath = path.join(moduleDirectory, model, 'routes.js');
-    let stats = fs.statSync(path.join(moduleDirectory, model));
-    if(!stats.isDirectory()) {
-      return;
-    }
-    if(fs.existsSync(routesPath)) {
-      require(routesPath)(router);
-    }
-    let routesFolder = path.join(moduleDirectory, model, "routes");
-    if(fs.existsSync(routesFolder)) {
-      fs.readdirSync(routesFolder).forEach(function(file) {
-        let st = fs.statSync(path.join(routesFolder, file));
-        if(st.isFile()) {
-          require(path.join(routesFolder, file))(router);
-        }
-      });
+    if(model !== "apps"){
+
+      
+      let routesPath = path.join(moduleDirectory, model, 'routes.js');
+      let stats = fs.statSync(path.join(moduleDirectory, model));
+      if(!stats.isDirectory()) {
+        return;
+      }
+      if(fs.existsSync(routesPath)) {
+        require(routesPath)(router);
+      }
+      let routesFolder = path.join(moduleDirectory, model, "routes");
+      if(fs.existsSync(routesFolder)) {
+        fs.readdirSync(routesFolder).forEach(function(file) {
+          let st = fs.statSync(path.join(routesFolder, file));
+          if(st.isFile()) {
+            require(path.join(routesFolder, file))(router);
+          }
+        });
+      }
     }
   });
 
@@ -39,6 +43,6 @@ module.exports = function(app) {
   });
 
   app.use((req, res)=>{
-        res.redirect('/');
-    });
+    res.redirect('/');
+  });
 }
